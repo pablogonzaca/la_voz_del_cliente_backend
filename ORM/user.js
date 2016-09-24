f7d2da14cdad8a177f59f408e88ce436ab213d52
Pablo Gonzalez <pablo.gonzalez.camacho@intel.com>
2016-09-24 11:53:04 -0600

    first commit

---
+++ ORM/user.js
@@ -0,0 +1,29 @@
+var sequelize = require('database');
+
+
+var User = sequelize.define('usuario', {
+  Usuario: {
+    type: Sequelize.STRING
+    // field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
+  },
+
+  NombreCompleto: {
+    type: Sequelize.STRING
+  },
+
+  Clave: {
+    type: Sequelize.STRING
+  },
+
+  EmailContacto: {
+    type: Sequelize.STRING
+  },
+
+  Id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true
+  }
+
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
